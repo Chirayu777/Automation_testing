04deb987e73298167f71a1a734b598fc5dee29bf
chirayu777 <chirayu@vithamastech.com>
2024-03-07 16:34:53 +0530

    static data in json

---
+++ roles.py
@@ -0,0 +1,306 @@
+# data_roles = [
+#     {
+#      "Roles ": "- React Developer",
+#      "Primary skills ": "React, JavaScript, TypeScript, ES6, HTML, CSS, React Router, Hooks, Redux, State Management, Unit Testing, RESTful API, GraphQL, Node.js, NPM, Webpack, Git, Component Libraries"
+#     },
+#     {
+#      "Roles ": "- Angular Developer",
+#      "Primary skills ": "Angular, TypeScript, RxJS, Angular CLI, Unit Testing, JavaScript, ES6, HTML, CSS, Node.js, NPM, Webpack, Git, RESTful API, GraphQL, Angular Material"
+#     },
+#     {
+#      "Roles ": "- Vue.js Developer",
+#      "Primary skills ": "Vue.js, JavaScript, ES6, HTML, CSS, Vue Router, Vuex, Single-File Components (SFC), Vue CLI, Node.js, NPM, Webpack, Git, RESTful API, TypeScript, GraphQL, Unit Testing"
+#     },
+#     {
+#      "Roles ": "- Ember.js Developer",
+#      "Primary skills ": "Ember.js, Ember CLI, Ember Data, Handlebars, Ember.js Addons, Routing and Controllers, JavaScript, ES6, HTML, CSS, Node.js, NPM, Git, RESTful API, Webpack"
+#     },
+#     {
+#      "Roles ": "- Svelte Developer",
+#      "Primary skills ": "Svelte, JavaScript, HTML, CSS, Svelte Components-Kit, Reactive with Svelte, Animations, Node.js, NPM, Git, RESTful API, Webpack, TypeScript, GraphQL, Unit Testing"
+#     },
+#     {
+#      "Roles ": "- JavaScript Developer",
+#      "Primary skills ": "JavaScript, HTML, CSS, Git, Node.js, NPM, ES6+, Asynchronous JavaScript, Web APIs, jQuery, Webpack, RESTful API, TypeScript, GraphQL, Unit Testing"
+#     },
+#     {
+#      "Roles ": "- UI Developer",
+#      "Primary skills ": "HTML, CSS, JavaScript, Responsive Design, Browser Developer Tools, Git, UI\/UX Design Principles, Sass, Less, Cross-Browser Compatibility"
+#     },
+#     {
+#      "Roles ": "- Node.js Developer",
+#      "Primary skills ": "Node.js, JavaScript, NPM, Express.js, RESTful APIs, Mocha, Jest, Git, MongoDB, Redis, MySQL, PostgreSQL"
+#     },
+#     {
+#      "Roles ": "- Python Developer",
+#      "Primary skills ": "Python, Flask, Django, FastAPI, SQL, Git, RESTful APIs, Pandas, NumPy, Scrapy, Pytest, Celery, GraphQL, Asyncio, Docker"
+#     },
+#     {
+#      "Roles ": "- Django Developer",
+#      "Primary skills ": "Python, Django, SQL, Git, RESTful APIs, Django REST Framework, Django ORM, Celery, Docker, Pytest, GraphQL, Asyncio, WebSockets"
+#     },
+#     {
+#      "Roles ": "- Ruby on Rails Developer",
+#      "Primary skills ": "Ruby, Ruby on Rails, SQL, Git, RESTful APIs, ActiveRecord, RSpec, Sidekiq, Docker, RSpec, GraphQL, WebSockets"
+#     },
+#     {
+#      "Roles ": "- Java Developer",
+#      "Primary skills ": "Java, Spring Framework, SQL, Git, RESTful APIs, Hibernate, JUnit, Maven, Docker, Spring Boot, Kafka, Microservices Architecture"
+#     },
+#     {
+#      "Roles ": "- .NET Developer",
+#      "Primary skills ": "C#, .NET Framework, ASP.NET, SQL, Git, RESTful APIs, Entity Framework, NUnit, Docker, ASP.NET Core, Message Queues, RabbitMQ, Microservices Architecture"
+#     },
+#     {
+#      "Roles ": "- PHP Developer",
+#      "Primary skills ": "PHP, MySQL, Git, RESTful APIs, Laravel, Composer, PHPUnit, Docker, MVC Architecture, SOLID Principles, Message Queues, RabbitMQ, Microservices Architecture"
+#     },
+#     {
+#      "Roles ": "- Go (Golang) Developer",
+#      "Primary skills ": "Golang, SQL, Git, RESTful APIs, Go Web Framework, Gin, Echo, Docker, Testing, Concurrency, Goroutines, Microservices Architecture, Kubernetes, Message Queues, RabbitMQ"
+#     },
+#     {
+#      "Roles ": "- Cloud Developer",
+#      "Primary skills ": "AWS, Azure, GCP, Containerization, Docker, Kubernetes, Terraform, CloudFormation, Microservices Architecture, Serverless Computing, RESTful APIs, SQL Databases, NoSQL Databases, Git, CI\/CD Pipelines, Monitoring Tools, Logging Tools, Security Best Practices, Networking, CDN, Automation, Scripting, Bash, Python"
+#     },
+#     {
+#      "Roles ": "- Serverless Developer",
+#      "Primary skills ": "Serverless, AWS Lambda, Azure Functions, Google Cloud Functions, AWS SAM, API Gateway, Microservices Architecture, DynamoDB, Cosmos DB, RESTful APIs, Git, CI\/CD Pipelines, Monitoring Tools, Logging Tools, Terraform, AWS CDK, Event-Driven Architectures, AWS, Azure, GCP, AWS Step Functions, Azure Logic Apps"
+#     },
+#     {
+#      "Roles ": "- MERN Stack Developer",
+#      "Primary skills ": "JavaScript, React, Node.js, Express.js, MongoDB, Git, RESTful APIs, HTML, CSS, Redux, Mongoose, NPM, Webpack, JSX, Responsive Design, MVC Architecture, Jest, Docker, CI\/CD Pipelines, GraphQL"
+#     },
+#     {
+#      "Roles ": "- MEAN Stack Developer",
+#      "Primary skills ": "JavaScript, Angular, Node.js, Express.js, MongoDB, Git, RESTful APIs, HTML, CSS, TypeScript, NPM, Webpack, Responsive Design, MVC Architecture, Unit Testing, Jasmine, Karma, Authentication, Authorization, Mongoose, Docker, CI\/CD Pipelines"
+#     },
+#     {
+#      "Roles ": "- LAMP Stack Developer",
+#      "Primary skills ": "Linux, Apache, MySQL, PHP, Git, RESTful APIs, HTML, CSS, JavaScript, MVC Architecture, SQL, Responsive Design, PHP Framework, Laravel, Symfony, ORM, Eloquent, Unit Testing, Web Server Configuration, Shell Scripting, CI\/CD Pipelines"
+#     },
+#     {
+#      "Roles ": "- Ruby on Rails Full-Stack Developer",
+#      "Primary skills ": "Ruby, Ruby on Rails, JavaScript, Git, RESTful APIs, HTML, CSS, SQL, ActiveRecord, Responsive Design, MVC Architecture, RSpec, React, Vue.js, Webpack, CI\/CD Pipelines"
+#     },
+#     {
+#      "Roles ": "- Java Full-Stack Developer",
+#      "Primary skills ": "Java, Spring Framework, JavaScript, Git, RESTful APIs, HTML, CSS, SQL, Hibernate, MVC Architecture, React, Angular, Vue.js, Build Tools, Maven, Gradle, Web Services, SOAP, REST, Spring Boot, JUnit, ORM, CI\/CD Pipelines"
+#     },
+#     {
+#      "Roles ": "- Python Full-Stack Developer",
+#      "Primary skills ": "Python, Django, Flask, JavaScript, Git, RESTful APIs, HTML, CSS, SQL, ORM, React, Vue.js, Jinja, MVC Architecture, Pytest, Web Services, SOAP, REST, CI\/CD Pipelines"
+#     },
+#     {
+#      "Roles ": "- .NET Full-Stack Developer",
+#      "Primary skills ": "C#, ASP.NET, .NET Core, JavaScript, Git, RESTful APIs, HTML, CSS, SQL, Entity Framework, Razor Pages, MVC Architecture, React, Angular, Vue.js, Blazor, MSTest, NUnit, Web Services, SOAP, REST, CI\/CD Pipelines"
+#     },
+#     {
+#      "Roles ": "- Polyglot Full-Stack Developer",
+#      "Primary skills ": "JavaScript, Python, Java, C#, Ruby, PHP, HTML, CSS, SQL, NoSQL Databases, MongoDB, RESTful APIs, Git, MVC Architecture, React, Angular, Vue.js, Django, Flask, Spring Boot, ASP.NET, Ruby on Rails, ORM, Jest, Pytest, JUnit, Web Services, SOAP, REST, Containerization, Docker, CI\/CD Pipelines, Responsive Design, AWS, Azure, GCP, Microservices Architecture, GraphQL"
+#     },
+#     {
+#      "Roles ": "- Automation Test Engineer",
+#      "Primary skills ": "Selenium, Appium, Java, Python, JUnit, TestNG, Pytest, CI\/CD Pipelines, Jenkins, API Testing, Postman, RestAssured, Mobile Testing, Cross-Browser Testing, Test Case Design, Agile Testing Methodologies, SDLC, STLC, Bugzilla, Jira"
+#     },
+#     {
+#      "Roles ": "- Manual Test Engineer",
+#      "Primary skills ": "Manual Testing, Test Case Design, Test Execution, Defect Tracking, Regression Testing, Smoke Testing, SDLC, STLC, Agile Testing Methodologies, Test Planning, Test Documentation, System Testing, User Acceptance Testing (UAT), Test Management Tools"
+#     },
+#     {
+#      "Roles ": "- Performance Test Engineer",
+#      "Primary skills ": "Load Testing, Stress Testing, JMeter, LoadRunner, Test Planning for Performance, Performance Metrics Analysis, Scalability Testing, Gatling, Grafana, InfluxDB, Jenkins, Git, New Relic, BlazeMeter, Agile"
+#     },
+#     {
+#      "Roles ": "- Security Test Engineer",
+#      "Primary skills ": "Security Testing, Vulnerability Assessment, Penetration Testing, Security Tools, Threat Modelling, OWASP ZAP, Burp Suite, Nessus, Wireshark, Metasploit, Kali Linux, Selenium, Git, JIRA, OWASP SAMM"
+#     },
+#     {
+#      "Roles ": "- Usability Test Engineer",
+#      "Primary skills ": "Usability Testing, Test Scenario Design, User-Centric Testing, Interaction Design Evaluation, User Interface (UI) Testing, UserZoom, Morae, UsabilityHub, Optimal Workshop, Lookback, Validately, Maze, Git, JIRA, Agile"
+#     },
+#     {
+#      "Roles ": "- Mobile Test Engineer",
+#      "Primary skills ": "Appium, Java\/Python, SDLC, Defect Tracking, Cross Browser Testing Skills, Xcode, Android Studio, Espresso, XCTest, Detox, AWS Device Farm, Git, JIRA, Agile"
+#     },
+#     {
+#      "Roles ": "- API Test Engineer",
+#      "Primary skills ": "API Testing, API Test Design, RESTful API Testing, API Automation, Postman, REST Assured, SoapUI, JMeter, Newman, Swagger, Git, JIRA, Agile"
+#     },
+#     {
+#      "Roles ": "- Site Reliability Engineer (SRE)",
+#      "Primary skills ": "Kubernetes, Terraform, Prometheus, Grafana, Docker, Ansible, Git, ELK Stack, Python, Agile"
+#     },
+#     {
+#      "Roles ": "- Security DevOps Engineer",
+#      "Primary skills ": "Automation, CI\/CD, Infrastructure as Code (IaC), Security Testing, Jenkins, GitLab, Docker, Kubernetes, Terraform, AWS Security Services, Chef, Puppet, Ansible, OWASP tools, Git, Python, Agile"
+#     },
+#     {
+#      "Roles ": "- Cloud DevOps Engineer",
+#      "Primary skills ": "AWS, Azure, Docker, Kubernetes, Ansible, GCP, Terraform, Jenkins, Git, Python, Monitoring Tools, Prometheus, Grafana, Agile"
+#     },
+#     {
+#      "Roles ": "- Containerisation and Kubernetes Engineer",
+#      "Primary skills ": "Containerization, Docker, Kubernetes Orchestration, Microservices Architecture, Container Security, Kubernetes Networking, Helm, Kubectl, Istio, Prometheus, Grafana, Git, Terraform, CI\/CD tools, YAML, Agile"
+#     },
+#     {
+#      "Roles ": "- CI\/CD Pipeline Engineer",
+#      "Primary skills ": "CI\/CD, Git, Jenkins, Automation, Scripting, Bash\/PowerShell, Testing, GitLab CI, Travis CI, CircleCI, Docker, Kubernetes, Ansible, Terraform, YAML, Agile"
+#     },
+#     {
+#      "Roles ": "- *iOS Developer*",
+#      "Primary skills ": "iOS SDK, Swift, REST API, UI\/UX Design, Core Data, MVC Architecture, Xcode, Git, Auto Layout, UIKit, SQL DB, CocoaPods, Alamofire, JIRA, Agile"
+#     },
+#     {
+#      "Roles ": "- *Android Developer*",
+#      "Primary skills ": "Android SDK, Kotlin, Java, REST API, UI\/UX Design, SQLite DB, MVC, Android Studio, Git, XML, Retrofit, Dagger, Gradle, ViewModel, LiveData, RecyclerView, Material Design, Jetpack Components, Firebase"
+#     },
+#     {
+#      "Roles ": "- React Native Developer",
+#      "Primary skills ": "React Native SDK, JavaScript, ReactJS, REST API, UI Design, SQL\/Database, MVC, TypeScript, Redux, Expo, NPM, Git, Visual Studio Code, JIRA, Agile"
+#     },
+#     {
+#      "Roles ": "- Flutter Developer",
+#      "Primary skills ": "Dart, Flutter, Material Design, REST API, SQL DB, Firebase, Bloc Pattern, Visual Studio Code, Git, JIRA, Agile"
+#     },
+#     {
+#      "Roles ": "- Xamarin Developer",
+#      "Primary skills ": "Xamarin.Forms, Xamarin.Native, C#, .NET, XAML, Xamarin.iOS, Xamarin.Android, Visual Studio, Git, JIRA, Agile"
+#     },
+#     {
+#      "Roles ": "- *Embedded Software Developer*",
+#      "Primary skills ": "Embedded Programming, C\/C++, RTOS, Linux, Drivers, Firmware, Hardware Interface,C\/C++,UART, SPI, I2C."
+#     },
+#     {
+#      "Roles ": "- *Hardware Engineer*",
+#      "Primary skills ": "Digital Circuit Design, Analog Circuit Design, PCB Design, FPGA Programming, Microcontroller\/Microprocessor Architecture, Signal Processing, Embedded Systems, Hardware Testing, Electronics Prototyping, VHDL, Verilog,Power Electronics.  \/ Verilog, VHDL, FPGA Design, ASIC Design, PCB Design, Signal Integrity Analysis"
+#     },
+#     {
+#      "Roles ": "- *Firmware Engineer*",
+#      "Primary skills ": "Embedded Programming, C\/C++, Firmware Development, RTOS, Device Drivers, Hardware Interface.  \/  Embedded Systems, Version Control ,JTAG, IDEs"
+#     },
+#     {
+#      "Roles ": "- *PCB Designer*",
+#      "Primary skills ": "PCB Design, Schematic Capture, Layout, EDA Tools, Signal Integrity, DFM, DFA, Prototyping, EMC\/EMI Compliance.  \/ Altium Designer, Eagle PCB, KiCad, Cadence Allegro, PCB Layout"
+#     },
+#     {
+#      "Roles ": "- *IoT Application Developer*",
+#      "Primary skills ": "IoT Protocols, Embedded Programming, Hardware Interface, Communication Tech, IoT Security, Data Analytics, Cloud Platforms, Edge Computing, App Development, Real-time Processing.\/MQTT, CoAP,AWS IoT, Azure IoT,MQTT Broker, IoT Security, Sensor Integration, MQTT.fx"
+#     },
+#     {
+#      "Roles ": "- AWS Developer",
+#      "Primary skills ": "AWS, CI\/CD, Docker, Kubernetes, Infrastructure as Code (IaC), AWS Lambda, Amazon S3, Amazon EC2, DynamoDB, Amazon RDS, AWS SDKs, AWS CloudFormation, Amazon API Gateway, IAM, Amazon CloudWatch, AWS CLI"
+#     },
+#     {
+#      "Roles ": "- Azure Developer",
+#      "Primary skills ": "Azure, CI\/CD, Docker, Kubernetes, Infrastructure as Code (IaC), Azure Logic Apps, Azure SDK, Azure Resource Manager (ARM)"
+#     },
+#     {
+#      "Roles ": "- Google Cloud Platform (GCP) Developer",
+#      "Primary skills ": "Google Cloud Storage, Compute Engine, App Engine, Google Kubernetes Engine (GKE), Cloud Functions, BigQuery, Pub\/Sub, Dataflow, Cloud SDK, Cloud SQL, Stackdriver"
+#     },
+#     {
+#      "Roles ": "- Data Analyst",
+#      "Primary skills ": "Data Analysis, SQL, Excel, Tableau, Power BI, Statistical Analysis, Python, R, Data Cleaning, Exploratory Data Analysis (EDA), Reporting, Data Warehousing"
+#     },
+#     {
+#      "Roles ": "- Data Engineer",
+#      "Primary skills ": "Data Engineering, ETL, Data Warehousing, Database Design, SQL, Python, Java, Big Data Technologies, Data Modelling, Data Integration, Cloud Platforms, Streaming Data Processing"
+#     },
+#     {
+#      "Roles ": "- Machine Learning (ML) Engineer",
+#      "Primary skills ": "Python, Applied Mathematics, TensorFlow, PyTorch, Statistics, Machine Learning Algorithms, Scikit-Learn, Deep Learning, Natural Language Processing (NLP), Computer Vision, Model Deployment, Data Preprocessing, Jupyter Notebooks"
+#     },
+#     {
+#      "Roles ": "- Data Scientist",
+#      "Primary skills ": "Python, R, Machine Learning, Data Analysis, Statistics, TensorFlow, PyTorch, Scikit-Learn, SQL, Big Data, Hadoop, Spark, Data Visualization, Matplotlib, Seaborn, Jupyter Notebooks"
+#     },
+#     {
+#      "Roles ": "- Business Analyst (BA)",
+#      "Primary skills ": "Requirements Analysis, Data Analysis, Stakeholder Management, Communication Skills, Tools, SQL, Microsoft Excel, Agile Methodologies, Use Case Development"
+#     },
+#     {
+#      "Roles ": "- Business Intelligence (BI) Analyst",
+#      "Primary skills ": "Data Analysis, Data Modeling, SQL, Tableau, Power BI, ETL (Extract, Transform, Load), Data Visualization, Dashboard Design, Reporting"
+#     },
+#     {
+#      "Roles ": "- Natural Language Processing (NLP) Engineer",
+#      "Primary skills ": "Python, NLP Libraries, NLTK, SpaCy, TensorFlow, PyTorch, Text Preprocessing, Named Entity Recognition, Text Classification, Word Embeddings, Transformers Architecture, Information Retrieval, Evaluation Metrics, Model Deployment"
+#     },
+#     {
+#      "Roles ": "- Data Engineer",
+#      "Primary skills ": "Apache Spark, Hadoop, SQL, Kafka, ETL, Data Warehousing, Python, Apache Hive, Data Modeling, DBMS"
+#     },
+#     {
+#      "Roles ": "- ETL Developer",
+#      "Primary skills ": "AWS, Azure, GCP, Hadoop, Spark, Oracle, MySQL, SQL Server, Python, Shell Scripting, ETL Tools, Informatica, Talend, SSIS, SQL, Data Warehousing"
+#     },
+#     {
+#      "Roles ": "- Big Data Engineer",
+#      "Primary skills ": "Hadoop, Spark, Hive, Pig, MapReduce, Kafka, NoSQL Databases, Apache Flink, YARN, Avro, Apache NiFi, Impala, Sqoop, SQL, Linux Shell Scripting, Cloud Platforms, Python, Java"
+#     },
+#     {
+#      "Roles ": "- Database Administrator (DBA)",
+#      "Primary skills ": "SQL, DBMS, Database Design, Query Optimization, Backup, Recovery, Performance Tuning, Indexing Strategies, Security Management, Data Migration, Database Monitoring, High Availability, Replication, SSMS, Oracle SQL Developer"
+#     },
+#     {
+#      "Roles ": "- Cloud Data Engineer",
+#      "Primary skills ": "Hadoop, Spark, AWS, Azure, GCP, Apache NiFi, Talend, Avro, Parquet, Cloud Platforms, SQL, ETL Tools, Cloud Storage"
+#     },
+#     {
+#      "Roles ": "- Data Engineer (PySpark)",
+#      "Primary skills ": "PySpark, Apache Spark, Hadoop, SQL, Kafka, Python, ETL, Data Warehousing, Apache Hive, Apache HBase, Data Modeling"
+#     },
+#     {
+#      "Roles ": "- Data Engineer (Scala Spark)",
+#      "Primary skills ": "Scala, Apache Spark, Hadoop, SQL, Kafka, ETL, Data Warehousing, Apache Hive, Apache HBase, Data Modeling"
+#     }
+#    ]
+
+# - React Developer
+# - Node.js Developer
+# - Python Developer
+# - Django Developer
+# - Java Back-end Developer
+# - MERN Stack Developer
+# - Automation Test Engineer
+# - React Native Developer
+# - Flutter Developer
+
+data_roles = [
+    {
+     "Roles ": "- React Developer",
+     "Primary skills ": "React, JavaScript, TypeScript, ES6, HTML, CSS, React Router, Hooks, Redux, State Management, Unit Testing, RESTful API, GraphQL, Node.js, NPM, Webpack, Git, Component Libraries"
+    },
+    {
+     "Roles ": "- Node.js Developer",
+     "Primary skills ": "Node.js, JavaScript, NPM, Express.js, RESTful APIs, Mocha, Jest, Git, MongoDB, Redis, MySQL, PostgreSQL"
+    },
+    {
+     "Roles ": "- Python Developer",
+     "Primary skills ": "Python, Flask, Django, FastAPI, SQL, Git, RESTful APIs, Pandas, NumPy, Scrapy, Pytest, Celery, GraphQL, Asyncio, Docker"
+    },
+    {
+     "Roles ": "- Django Developer",
+     "Primary skills ": "Python, Django, SQL, Git, RESTful APIs, Django REST Framework, Django ORM, Celery, Docker, Pytest, GraphQL, Asyncio, WebSockets"
+    },
+    {
+     "Roles ": "- MERN Stack Developer",
+     "Primary skills ": "JavaScript, React, Node.js, Express.js, MongoDB, Git, RESTful APIs, HTML, CSS, Redux, Mongoose, NPM, Webpack, JSX, Responsive Design, MVC Architecture, Jest, Docker, CI\/CD Pipelines, GraphQL"
+    },
+    {
+     "Roles ": "- Automation Test Engineer",
+     "Primary skills ": "Selenium, Appium, Java, Python, JUnit, TestNG, Pytest, CI\/CD Pipelines, Jenkins, API Testing, Postman, RestAssured, Mobile Testing, Cross-Browser Testing, Test Case Design, Agile Testing Methodologies, SDLC, STLC, Bugzilla, Jira"
+    },
+    {
+     "Roles ": "- React Native Developer",
+     "Primary skills ": "React Native SDK, JavaScript, ReactJS, REST API, UI Design, SQL\/Database, MVC, TypeScript, Redux, Expo, NPM, Git, Visual Studio Code, JIRA, Agile"
+    },
+    {
+     "Roles ": "- Flutter Developer",
+     "Primary skills ": "Dart, Flutter, Material Design, REST API, SQL DB, Firebase, Bloc Pattern, Visual Studio Code, Git, JIRA, Agile"
+    },
+
+]
+
+
